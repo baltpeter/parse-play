4cfcce3372d81922556ce1eda26edb44a7fb1a87
Benjamin Altpeter <benjamin@oldpeter.de>
2022-06-15 21:24:34 +0200

    Allow batching multiple requests

---
+++ src/common/requests.ts
@@ -0,0 +1,30 @@
+import fetch from 'cross-fetch';
+import { assert } from './assert';
+
+export type RequestPayload = [string, string];
+export type QueryParams = Record<string, string>;
+
+const buildQueryString = (queryParams?: QueryParams) =>
+    queryParams
+        ? `?${Object.entries(queryParams)
+              .map(([key, value]) => `${key}=${value}`)
+              .join('&')}`
+        : '';
+
+export const batchExecute = async (requests: RequestPayload[], queryParams?: QueryParams) => {
+    const res = await fetch(`https://play.google.com/_/PlayStoreUi/data/batchexecute${buildQueryString(queryParams)}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        },
+        // The (stringified) number at index 3 of each request specifies the order in which the responses are returned
+        // (see: https://kovatch.medium.com/deciphering-google-batchexecute-74991e4e446c#ea4a). We want them in the same
+        // order as the requests.
+        body: `f.req=${encodeURIComponent(JSON.stringify([requests.map((r, idx) => [...r, null, '' + idx])]))}`,
+    }).then((r) => r.text());
+
+    const messages: any[] = JSON.parse(res.split('\n')[2]);
+    assert(messages.length === requests.length + 2, 'Has response payload for each request.');
+
+    return messages.slice(0, requests.length);
+};
--- src/top-charts.ts
+++ src/top-charts.ts
@@ -1,36 +1,46 @@
-import fetch from 'cross-fetch';
+import { batchExecute, RequestPayload } from './common/requests';
 import { CategoryId, LanguageCode, CountryCode } from './common/consts';
 import { assert } from './common/assert';
 
-export const fetchTopCharts = async (options: {
+export type TopChartsRequest = {
     chart: 'topselling_free' | 'topgrossing' | 'topselling_paid';
     category: CategoryId;
     count: number;
+};
+export type TopChartsOptions = {
     country: CountryCode;
     language: LanguageCode;
-}) => {
-    // This payload was determined by observing the network traffic on the web UI and then _drastically_ simplifying it
-    // by throwing away everything that didn't affect the response.
-    const requestPayload = [
-        'vyAe2',
-        JSON.stringify([
-            [null, [[null, [null, options.count]], null, null, [113]], [2, options.chart, options.category]],
-        ]),
-    ];
+};
 
-    const res = await fetch(
-        `https://play.google.com/_/PlayStoreUi/data/batchexecute?hl=${options.language}&gl=${options.country}`,
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-            },
-            body: `f.req=${encodeURIComponent(JSON.stringify([[requestPayload]]))}`,
-        }
-    ).then((r) => r.text());
+export type TopChartsEntry = ReturnType<typeof parseTopChartEntry>;
+export type TopChartsResult = TopChartsEntry[];
 
-    const payload = JSON.parse(res.split('\n')[2])[0];
-    assert(payload, 'Has payload.');
+// This payload was determined by observing the network traffic on the web UI and then _drastically_ simplifying it
+// by throwing away everything that didn't affect the response.
+export const topChartsRequestPayload = (options: TopChartsRequest): RequestPayload => [
+    'vyAe2',
+    JSON.stringify([[null, [[null, [null, options.count]], null, null, [113]], [2, options.chart, options.category]]]),
+];
+
+export const parseTopChartEntry = (entry: any, idx: number) => ({
+    position: (idx + 1) as number,
+    app_id: entry[0][0] as string,
+    icon_url: entry[1][3][2] as string,
+    screenshot_urls: entry[2].map((s) => s[3][2]) as string[],
+    name: entry[3] as string,
+    rating: entry[4][1] as number,
+    category: entry[5] as string,
+    price: entry[8][1][0].join(' ').trim() as string,
+    buy_url: entry[8][6][5][2] as string,
+    store_path: entry[10][4][2] as string,
+    trailer_url: entry[12]?.[0][0][3][2] as string | undefined,
+    description: entry[13][1] as string,
+    developer: entry[14] as string,
+    downloads: entry[15] as string,
+    cover_image_url: entry[22][3][2] as string,
+});
+
+export const parseTopChartPayload = (payload: any): TopChartsEntry[] => {
     assert(payload[0] === 'wrb.fr' && payload[1] === 'vyAe2', 'Correct header.');
 
     const data = JSON.parse(payload[2]);
@@ -43,7 +53,7 @@ export const fetchTopCharts = async (options: {
             data[0][1][0].filter((i: unknown) => i === null).length === 27,
         'Expected inner data structure.'
     );
-    const entries = data[0][1][0][28][0];
+    const entries: any[] = data[0][1][0][28][0];
     assert(entries.length > 0, 'Has data.');
 
     const parsed = entries.map((e, idx) => {
@@ -60,23 +70,7 @@ export const fetchTopCharts = async (options: {
             'Weird second meta object only has category.'
         );
 
-        return {
-            position: idx + 1,
-            app_id: meta[0][0],
-            icon_url: meta[1][3][2],
-            screenshot_urls: meta[2].map((s) => s[3][2]),
-            name: meta[3],
-            rating: meta[4][1],
-            category: meta[5],
-            price: meta[8][1][0].join(' ').trim(),
-            buy_url: meta[8][6][5][2],
-            store_path: meta[10][4][2],
-            trailer_url: meta[12]?.[0][0][3][2],
-            description: meta[13][1],
-            developer: meta[14],
-            downloads: meta[15],
-            cover_image_url: meta[22][3][2],
-        };
+        return parseTopChartEntry(meta, idx);
     });
 
     assert(
@@ -90,3 +84,21 @@ export const fetchTopCharts = async (options: {
 
     return parsed;
 };
+
+export async function fetchTopCharts(
+    request: TopChartsRequest | [TopChartsRequest],
+    options: TopChartsOptions
+): Promise<TopChartsResult>;
+export async function fetchTopCharts(
+    requests: TopChartsRequest[],
+    options: TopChartsOptions
+): Promise<TopChartsResult[]>;
+export async function fetchTopCharts(requests: TopChartsRequest | TopChartsRequest[], options: TopChartsOptions) {
+    const _requests = Array.isArray(requests) ? requests : [requests];
+    const payloads = await batchExecute(
+        _requests.map((r) => topChartsRequestPayload(r)),
+        { hl: options.language, gl: options.country }
+    );
+    const res = payloads.map((p) => parseTopChartPayload(p));
+    return _requests.length === 1 ? res[0] : res;
+}
